74d7e942ad0d122a76cc1b74909d7ce5b71ba031
wsttiger <wsttiger@gmail.com>
2017-08-23 20:08:16 +0000

    Added constructors and destructors to THCUNN functors

--- aten/src/THCUNN/Sqrt.cu
+++ aten/src/THCUNN/Sqrt.cu
@@ -8,6 +8,18 @@ struct sqrtupdateOutput_functor
 {
   const T bias;
 
+#if defined(__HIP_PLATFORM_HCC__)
+  __host__ __device__
+  sqrtupdateOutput_functor() = default;
+
+  __host__ __device__
+  sqrtupdateOutput_functor(const sqrtupdateOutput_functor& f) = default;
+
+  __host__ __device__
+  ~sqrtupdateOutput_functor() {}
+
+  __host__ __device__
+#endif
   sqrtupdateOutput_functor(T bias_)
     : bias(bias_)
   {}
@@ -21,12 +33,20 @@ struct sqrtupdateOutput_functor
 template <typename T>
 struct sqrtupdateGradInput_functor
 {
+#if defined(__HIP_PLATFORM_HCC__)
+  __host__ __device__
+#endif
   sqrtupdateGradInput_functor() {}
 
   __device__ void operator()(T *gradInput, const T *output, const T *gradOutput) const
   {
     *gradInput = (THCNumerics<T>::eq(*output,ScalarConvert<float, T>::to(0.0f))) ? ScalarConvert<float, T>::to(0.0f) : ((ScalarConvert<float, T>::to(0.5f) * *gradOutput) / *output);
   }
+
+#if defined(__HIP_PLATFORM_HCC__)
+  __host__ __device__
+  ~sqrtupdateGradInput_functor() {}
+#endif
 };
 
 #include "generic/Sqrt.cu"
