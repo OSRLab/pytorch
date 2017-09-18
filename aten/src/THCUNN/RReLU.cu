82d8be21b67af4ba4b015938636408a9751c2c15
wsttiger <wsttiger@gmail.com>
2017-09-18 23:43:45 +0000

    Fixed more unresolved references in THCUNN on ROCM software stack

--- aten/src/THCUNN/RReLU.cu
+++ aten/src/THCUNN/RReLU.cu
@@ -1,8 +1,8 @@
-#if defined(__NVCC__)
 #include "THCUNN.h"
 #include "THCHalf.h"
 #include "THCHalfAutoNumerics.cuh"
 #include <THC/THCApply.cuh>
+#if defined(__NVCC__)
 #include "common.h"
 #include <curand.h>
 #include <curand_kernel.h>
@@ -120,7 +120,7 @@ struct RReLUupdateGradInputEvalIP_functor
     }
   }
 };
+#endif
 
 #include "generic/RReLU.cu"
 #include "THCGenerateFloatTypes.h"
-#endif
