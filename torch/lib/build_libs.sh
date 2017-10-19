9c5c26df83bcaaf7fba843a732f1d1a38afa69dc
wsttiger <wsttiger@gmail.com>
2017-10-19 21:08:38 +0000

    Added build_rocm_ATen function for ROCM build

--- torch/lib/build_libs.sh
+++ torch/lib/build_libs.sh
@@ -349,6 +349,58 @@ function build_rocm_THCS() {
     cd ../..
   fi
 }
+function build_rocm_ATen() {
+  # We create a build directory for the library, which will
+  # contain the cmake output
+  mkdir -p build/ATen
+  cd build/ATen
+  BUILD_C_FLAGS=''
+  case ATen in
+      THCS | THCUNN ) BUILD_C_FLAGS=$C_FLAGS;;
+      *) BUILD_C_FLAGS=$C_FLAGS" -fexceptions";;
+  esac
+  cmake ../../ATen -DCMAKE_MODULE_PATH="/opt/rocm/hip/cmake" \
+              -DTorch_FOUND="1" \
+              -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+              -DCMAKE_C_FLAGS="$BUILD_C_FLAGS" \
+              -DCMAKE_CXX_FLAGS="$BUILD_C_FLAGS $CPP_FLAGS" \
+              -DCMAKE_EXE_LINKER_FLAGS="$LDFLAGS" \
+              -DCMAKE_SHARED_LINKER_FLAGS="$LDFLAGS" \
+              -DCUDA_NVCC_FLAGS="$C_FLAGS" \
+              -DTH_INCLUDE_PATH="$INSTALL_DIR/include" \
+              -DTH_LIB_PATH="$INSTALL_DIR/lib" \
+              -DTH_LIBRARIES="$INSTALL_DIR/lib/libTH$LD_POSTFIX" \
+              -DTHPP_LIBRARIES="$INSTALL_DIR/lib/libTHPP$LD_POSTFIX" \
+              -DATEN_LIBRARIES="$INSTALL_DIR/lib/libATen$LD_POSTFIX" \
+              -DTHNN_LIBRARIES="$INSTALL_DIR/lib/libTHNN$LD_POSTFIX" \
+              -DTHCUNN_LIBRARIES="$INSTALL_DIR/lib/libTHCUNN$LD_POSTFIX" \
+              -DTHS_LIBRARIES="$INSTALL_DIR/lib/libTHS$LD_POSTFIX" \
+              -DTHC_LIBRARIES="$INSTALL_DIR/lib/libTHC$LD_POSTFIX" \
+              -DTHCS_LIBRARIES="$INSTALL_DIR/lib/libTHCS$LD_POSTFIX" \
+              -DTH_SO_VERSION=1 \
+              -DTHC_SO_VERSION=1 \
+              -DTHNN_SO_VERSION=1 \
+              -DTHCUNN_SO_VERSION=1 \
+              -DTHD_SO_VERSION=1 \
+              -DWITH_ROCM=1 \
+              -DCMAKE_BUILD_TYPE=$([ $DEBUG ] && echo Debug || echo Release)
+  make install -j$(getconf _NPROCESSORS_ONLN)
+  cd ../..
+
+  local lib_prefix=$INSTALL_DIR/lib/libATen
+  if [ -f "$lib_prefix$LD_POSTFIX" ]; then
+    rm -rf -- "$lib_prefix$LD_POSTFIX_UNVERSIONED"
+  fi
+
+  if [[ $(uname) == 'Darwin' ]]; then
+    cd tmp_install/lib
+    for lib in *.dylib; do
+      echo "Updating install_name for $lib"
+      install_name_tool -id @rpath/$lib $lib
+    done
+    cd ../..
+  fi
+}
 function build_nccl() {
    mkdir -p build/nccl
    cd build/nccl
@@ -441,11 +493,38 @@ if [[ $WITH_ROCM -eq 1 ]]; then
     fi
     cd ..
 fi
-if [[ $WITH_ROCM -eq 1 ]]; then
-    build_rocm_THC
-    build_rocm_THCUNN
-    build_rocm_THCS
-fi
+for arg in "$@"; do
+    if [[ $WITH_ROCM -eq 1 ]]; then
+        if [[ "$arg" == "THC" ]]; then
+            build_rocm_THC
+        elif [[ "$arg" == "THCUNN" ]]; then
+            build_rocm_THCUNN
+        elif [[ "$arg" == "THCS" ]]; then
+            build_rocm_THCS
+        elif [[ "$arg" == "ATen" ]]; then
+            build_rocm_ATen
+        elif [[ "$arg" == "nccl" ]]; then
+            build_nccl
+        elif [[ "$arg" == "gloo" ]]; then
+            build gloo $GLOO_FLAGS
+        else
+            build $arg
+        fi
+    else
+        if [[ "$arg" == "nccl" ]]; then
+            build_nccl
+        elif [[ "$arg" == "gloo" ]]; then
+            build gloo $GLOO_FLAGS
+        else
+            build $arg
+        fi
+    fi
+done
+# if [[ $WITH_ROCM -eq 1 ]]; then
+#     build_rocm_THC
+#     build_rocm_THCUNN
+#     build_rocm_THCS
+# fi
 
 # If all the builds succeed we copy the libraries, headers,
 # binaries to torch/lib
