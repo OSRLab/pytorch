a2129e45ee99fd9ed691d7dc91574095506ad7de
wsttiger <wsttiger@gmail.com>
2017-10-20 17:14:23 +0000

    Re-enabled *FullDilatedConvolution functionality on the HCC / ROCM path

--- torch/lib/THCUNN/generic/VolumetricFullConvolution.cu
+++ torch/lib/THCUNN/generic/VolumetricFullConvolution.cu
@@ -14,11 +14,9 @@ void THNN_(VolumetricFullConvolution_updateOutput)(
        int padT, int padW, int padH,
        int adjT, int adjW, int adjH)
 {
-#if !defined(__HIP_PLATFORM_HCC__)
   THNN_(VolumetricFullDilatedConvolution_updateOutput)(
        state, input, output, weight, bias, finput, fgradInput,
        dT, dW, dH, padT, padW, padH, 1, 1, 1, adjT, adjW, adjH);
-#endif
 }
 
 void THNN_(VolumetricFullConvolution_updateGradInput)(
@@ -33,11 +31,9 @@ void THNN_(VolumetricFullConvolution_updateGradInput)(
        int padT, int padW, int padH,
        int adjT, int adjW, int adjH)
 {
-#if !defined(__HIP_PLATFORM_HCC__)
   THNN_(VolumetricFullDilatedConvolution_updateGradInput)(
        state, input, gradOutput, gradInput, weight, finput, fgradInput,
        dT, dW, dH, padT, padW, padH, 1, 1, 1, adjT, adjW, adjH);
-#endif
 }
 
 
@@ -54,11 +50,9 @@ void THNN_(VolumetricFullConvolution_accGradParameters)(
            int adjT, int adjW, int adjH,
            accreal scale_)
 {
-#if !defined(__HIP_PLATFORM_HCC__)
   THNN_(VolumetricFullDilatedConvolution_accGradParameters)(
        state, input, gradOutput, gradWeight, gradBias, finput, fgradInput,
        dT, dW, dH, padT, padW, padH, 1, 1, 1, adjT, adjW, adjH, scale_);
-#endif
 }
 
 #endif
