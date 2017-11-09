ff6c3d1d19cefc7b85a0055e119f650f9884bfdd
Soumith Chintala <soumith@gmail.com>
2017-11-09 07:58:52 -0800

    add warnings if device capability is less than ideal for compiled cuda version

--- torch/csrc/cuda/Module.cpp
+++ torch/csrc/cuda/Module.cpp
@@ -112,6 +112,11 @@ PyObject * THCPModule_getCompiledVersion(PyObject *self)
   return PyLong_FromLong((long) CUDA_VERSION);
 }
 
+PyObject * THCPModule_getCompiledVersion(PyObject *self)
+{
+  return PyLong_FromLong((long) CUDA_VERSION);
+}
+
 PyObject * THCPModule_getRNGState(PyObject *_unused)
 {
   using namespace at;
