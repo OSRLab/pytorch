777e2240636ffec3c79780db692f8236fc289c40
wsttiger <wsttiger@gmail.com>
2017-11-15 16:17:54 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -264,6 +264,12 @@ class build_deps(Command):
         check_pydep('typing', 'typing')
 
         libs = []
+
+        libs = ['TH', 'THS', 'THNN']
+        if WITH_CUDA:
+            libs += ['THC', 'THCS', 'THCUNN']
+        if WITH_ROCM:
+            libs += ['THC', 'THCS', 'THCUNN']
         if WITH_NCCL and not WITH_SYSTEM_NCCL:
             libs += ['nccl']
         libs += ['ATen', 'nanopb']
@@ -277,6 +283,11 @@ class build_deps(Command):
             libs += ['THD']
         build_libs(libs)
 
+        if WITH_ROCM:
+            os.environ["CC"] = 'hipcc'
+            os.environ["CXX"] = 'hipcc'
+            os.environ["LDSHARED"] = 'hcc'
+            extra_link_args.append('-shared')
 
 build_dep_cmds = {}
 
@@ -689,11 +700,6 @@ if WITH_ROCM:
     extra_link_args.append('')
     extra_compile_args += ['-DWITH_ROCM']
     extra_compile_args += ['-D__HIP_PLATFORM_HCC__']
-
-    os.environ["CC"] = 'hipcc'
-    os.environ["CXX"] = 'hipcc'
-    os.environ["LDSHARED"] = 'hcc'
-    extra_link_args.append('-shared')
 
     # main_libraries += []
     main_link_args += [THC_LIB, THCS_LIB, THCUNN_LIB]
