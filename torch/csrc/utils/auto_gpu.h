2b84b2976d14700eeb2601cc54a5c928721b19b3
Jorg Doku <jd728@cornell.edu>
2018-01-31 17:35:35 -0800

    Fixed the preprocessor declarations in the module files in order to prevent the WITH_CUDA from blocking out all of the hip libraries and functions.

--- torch/csrc/utils/auto_gpu.h
+++ torch/csrc/utils/auto_gpu.h
@@ -8,12 +8,10 @@
 #include <ATen/ATen.h>
 
 #ifdef WITH_CUDA
-  #if defined(__HIP_PLATFORM_HCC__)
-    #include <hip/hip_runtime.h>
-  #else
-    #include <cuda.h>
-    #include <cuda_runtime.h>
-  #endif
+  #include <cuda.h>
+  #include <cuda_runtime.h>
+#elif defined(__HIP_PLATFORM_HCC__)
+  #include <hip/hip_runtime.h>
 #endif
 
 struct AutoGPU {
@@ -33,50 +31,60 @@ struct AutoGPU {
   }
 
   ~AutoGPU() {
-#ifdef WITH_CUDA
     if (original_device != -1) {
-#if defined(__HIP_PLATFORM_HCC__)
+#ifdef WITH_CUDA
+      cudaSetDevice(original_device);
+#elif defined(__HIP_PLATFORM_HCC__)
       hipSetDevice(original_device);
 #else
-      cudaSetDevice(original_device);
 #endif
     }
-#endif
   }
 
   inline void setDevice(int device) {
-#ifdef WITH_CUDA
     if (device == -1) {
       return;
     }
+
     if (original_device == -1) {
-#if defined(__HIP_PLATFORM_HCC__)
+#ifdef WITH_CUDA
+      cudaCheck(cudaGetDevice(&original_device));
+#elif defined(__HIP_PLATFORM_HCC__)
       cudaCheck(hipGetDevice(&original_device));
 #else
-      cudaCheck(cudaGetDevice(&original_device));
 #endif
       if (device != original_device) {
-#if defined(__HIP_PLATFORM_HCC__)
+#ifdef WITH_CUDA
+        cudaCheck(cudaSetDevice(device));
+#elif defined(__HIP_PLATFORM_HCC__)
         cudaCheck(hipSetDevice(device));
 #else
-        cudaCheck(cudaSetDevice(device));
 #endif
       }
     } else {
-#if defined(__HIP_PLATFORM_HCC__)
+#ifdef WITH_CUDA
+      cudaCheck(cudaSetDevice(device));
+#elif defined(__HIP_PLATFORM_HCC__)
       cudaCheck(hipSetDevice(device));
 #else
-      cudaCheck(cudaSetDevice(device));
 #endif
     }
-#endif
   }
 
   int original_device = -1;
 
 private:
 #ifdef WITH_CUDA
-#if defined(__HIP_PLATFORM_HCC__)
+  static void cudaCheck(cudaError_t err) {
+    if (err != cudaSuccess) {
+      std::string msg = "CUDA error (";
+      msg += std::to_string(err);
+      msg += "): ";
+      msg += cudaGetErrorString(err);
+      throw std::runtime_error(msg);
+    }
+  }
+#elif defined(__HIP_PLATFORM_HCC__)
   static void cudaCheck(hipError_t err) {
     if (err != hipSuccess) {
       std::string msg = "CUDA error (";
@@ -87,15 +95,5 @@ private:
     }
   }
 #else
-  static void cudaCheck(cudaError_t err) {
-    if (err != cudaSuccess) {
-      std::string msg = "CUDA error (";
-      msg += std::to_string(err);
-      msg += "): ";
-      msg += cudaGetErrorString(err);
-      throw std::runtime_error(msg);
-    }
-  }
-#endif
 #endif
 };
