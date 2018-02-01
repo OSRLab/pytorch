12cc85c85da2d4b3a954a766657c2faae67f3cc1
Jorg Doku <jd728@cornell.edu>
2018-01-31 16:57:37 -0800

    make_magic_wrapper was a hack that allowed us to send structs with arbitrary sized arrays BY VALUE to kernels. This was done by storing these in pinned memory. However, this has been resolved and now make_magic_wrapper is not necessary.

--- aten/src/THC/generic/THCTensorTopK.cu
+++ aten/src/THC/generic/THCTensorTopK.cu
@@ -33,17 +33,17 @@ THC_API void THCTensor_(topk)(THCState* state,
     hipLaunchKernelGGL(                                                   \
       (gatherTopK<real, INDEX_T, DIM, DIR>),                              \
         grid, block, 0, THCState_getCurrentStream(state),                 \
-        make_magic_wrapper(inputInfo),                                    \
+        inputInfo,                                    \
         sliceSize,                                                        \
         k,                                                                \
         inputSlices,                                                      \
         /* The actual dimension that the k-selection is running in */     \
         /* may have changed from collapseDims() */                        \
         inputInfo.strides[collapseInputDim],                              \
-        make_magic_wrapper(topKInfo),                                     \
+        topKInfo,                                     \
         topKSlices,                                                       \
         topKInfo.strides[collapseTopKDim],                                \
-        make_magic_wrapper(indicesInfo),                                  \
+        indicesInfo,                                  \
         indicesInfo.strides[collapseIndicesDim])
 #else
   #define RUN_K(INDEX_T, DIM, DIR)                                        \
