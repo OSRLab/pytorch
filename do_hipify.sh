9f6a12ba011992e75c421351201276f19aca2db2
Jorg Doku <jd728@cornell.edu>
2018-02-17 18:57:41 -0800

    Updated the do_hipify script.

--- do_hipify.sh
+++ do_hipify.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-cd torch/lib
+cd aten/src/
 
-# THC
+##### THC Files ####
 mkdir -p THC/hip
 mkdir -p THC/hip/generic
 cp THC/*.h THC/hip/
@@ -12,26 +12,32 @@ cp THC/*.c THC/hip/
 cp THC/*.cpp THC/hip/
 cp THC/*.cu THC/hip/
 cp THC/*.cuh THC/hip/
+
+# THC Generic Files
 cp THC/generic/*.h THC/hip/generic/
 cp THC/generic/*.c THC/hip/generic/
 cp THC/generic/*.cu THC/hip/generic/
+
+# THC HIP Files
 cp THC/CMakeLists.txt.hip THC/hip/CMakeLists.txt
-cp THC/THCGeneral.h.in.hip THC/hip/THCGeneral.h.in
-cp THC/THCBlas.cu.hip THC/hip/THCBlas.cu
+cp THC/THCAllocator.c.hip THC/hip/THCAllocator.c
 cp THC/THCApply.cuh.hip THC/hip/THCApply.cuh
+cp THC/THCBlas.cu.hip THC/hip/THCBlas.cu
+cp THC/THCGeneral.cpp.hip THC/hip/THCGeneral.cpp
+cp THC/THCGeneral.h.in.hip THC/hip/THCGeneral.h.in
+cp THC/THCNumerics.cuh.hip THC/hip/THCNumerics.cuh
 cp THC/THCTensorRandom.cu.hip THC/hip/THCTensorRandom.cu
 cp THC/THCTensorRandom.cuh.hip THC/hip/THCTensorRandom.cuh
 cp THC/THCTensorRandom.h.hip THC/hip/THCTensorRandom.h
-cp THC/THCNumerics.cuh.hip THC/hip/THCNumerics.cuh
-cp THC/generic/THCTensorRandom.cu.hip THC/hip/generic/THCTensorRandom.cu
-cp THC/THCGeneral.cc.hip THC/hip/THCGeneral.cc
-cp THC/THCAllocator.cc.hip THC/hip/THCAllocator.cc
 cp THC/generic/THCStorage.c.hip THC/hip/generic/THCStorage.c
+cp THC/generic/THCTensorRandom.cu.hip THC/hip/generic/THCTensorRandom.cu
+
+# Run hipify script in place
 /opt/rocm/hip/bin/hipconvertinplace-perl.sh THC/hip/
 /opt/rocm/hip/bin/hipify-perl THC/hip/THCGeneral.h.in
 find THC/hip -name "*.prehip" -type f -delete
 
-# THCUNN
+##### THCUNN Files ####
 mkdir -p THCUNN/hip
 mkdir -p THCUNN/hip/generic
 cp THCUNN/*.h THCUNN/hip/
@@ -43,14 +49,14 @@ cp THCUNN/CMakeLists.txt.hip THCUNN/hip/CMakeLists.txt
 /opt/rocm/hip/bin/hipconvertinplace-perl.sh THCUNN/hip/
 find THCUNN/hip -name "*.prehip" -type f -delete
 
-# THCS
+#### THCS Files ####
 mkdir -p THCS/hip
 mkdir -p THCS/hip/generic
 cp THCS/*.h THCS/hip/
-cp THCS/*.c THCS/hip/
+cp THCS/*.cpp THCS/hip/
 cp THCS/*.cu THCS/hip/
 cp THCS/generic/*.h THCS/hip/generic/
-cp THCS/generic/*.c THCS/hip/generic/
+cp THCS/generic/*.cpp THCS/hip/generic/
 cp THCS/generic/*.cu THCS/hip/generic/
 cp THCS/CMakeLists.txt.hip THCS/hip/CMakeLists.txt
 /opt/rocm/hip/bin/hipconvertinplace-perl.sh THCS/hip/
@@ -59,8 +65,20 @@ find THCS/hip -name "*.prehip" -type f -delete
 # Make link directories
 mkdir -p HIP
 cd HIP
+if [ ! -L "TH" ]; then
+    ln -s ../TH TH
+fi
+if [ ! -L "THS" ]; then
+    ln -s ../THS THS
+fi
+if [ ! -L "THNN" ]; then
+    ln -s ../THNN THNN
+fi
 if [ ! -L "THC" ]; then
     ln -s ../THC/hip THC
+fi
+if [ ! -L "THCS" ]; then
+    ln -s ../THCS/hip THCS
 fi
 if [ ! -L "THCUNN" ]; then
     ln -s ../THCUNN/hip THCUNN
@@ -70,9 +88,6 @@ if [ ! -L "THD" ]; then
 fi
 if [ ! -L "THPP" ]; then
     ln -s ../THPP THPP
-fi
-if [ ! -L "THS" ]; then
-    ln -s ../THS THS
 fi
 if [ ! -L "ATen" ]; then
     ln -s ../ATen ATen
