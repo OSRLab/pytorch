7904ae85ccb115339d5504a23e1f44ff4fa21463
Jorg Doku <jd728@cornell.edu>
2018-02-19 18:36:42 -0800

    Updated Build Style

--- do_hipify.sh
+++ do_hipify.sh
@@ -12,7 +12,7 @@ cd aten/hip-src/
 cp ../CMakeLists.txt.hip ../CMakeLists.txt
 
 # Extract the THC (.hip) files.
-# cp THC/CMakeLists.txt.hip THC/CMakeLists.txt
+cp THC/CMakeLists.txt.hip THC/CMakeLists.txt
 cp THC/THCAllocator.c.hip THC/THCAllocator.c
 cp THC/THCApply.cuh.hip THC/THCApply.cuh
 cp THC/THCBlas.cu.hip THC/THCBlas.cu
@@ -31,12 +31,12 @@ cp THC/generic/THCTensorRandom.cu.hip THC/generic/THCTensorRandom.cu
 find THC/ -name "*.prehip" -type f -delete
 
 # Extract the THCUNN (.hip) files.
-# cp THCUNN/CMakeLists.txt.hip THCUNN/CMakeLists.txt
+cp THCUNN/CMakeLists.txt.hip THCUNN/CMakeLists.txt
 /opt/rocm/hip/bin/hipconvertinplace-perl.sh THCUNN/
 find THCUNN/ -name "*.prehip" -type f -delete
 
 # Extract the THCS (.hip) files.
-# cp THCS/CMakeLists.txt.hip THCS/CMakeLists.txt
+cp THCS/CMakeLists.txt.hip THCS/CMakeLists.txt
 /opt/rocm/hip/bin/hipconvertinplace-perl.sh THCS/
 find THCS/ -name "*.prehip" -type f -delete
 
