f5c3981c4fff49169a6aea524309b80fe098f129
Jorg Doku <jd728@cornell.edu>
2018-02-19 16:48:34 -0800

    Updating build style to start from aten.

--- setup.py
+++ setup.py
@@ -806,6 +806,7 @@ if not IS_WINDOWS:
     DL = Extension("torch._dl",
                    sources=["torch/csrc/dl.c"],
                    language='c',
+                   extra_link_args=["-shared"],
                    )
     extensions.append(DL)
 
