856fdda09aaac06bd822bff2ec16ebe54d8a71b9
Jorg Doku <jd728@cornell.edu>
2018-03-07 13:40:01 -0800

    Using GCC atomics now

--- aten/src/ATen/native/cuda/Embedding.cu
+++ aten/src/ATen/native/cuda/Embedding.cu
@@ -317,10 +317,9 @@ Tensor embedding_backward_cuda(const Tensor & grad_, const Tensor & indices,
       grad.data<cuda_scalar_t>(),
       grad_weight.data<cuda_scalar_t>(),
       count.defined() ? count.data<int64_t>() : nullptr,
-      num_indices,
-      stride,
-      padding_idx);
-#endif
+      static_cast<int64_t>(num_indices),
+      static_cast<int64_t>(stride),
+      static_cast<int>(padding_idx));
   });
   THCudaCheck(cudaGetLastError());
 
@@ -374,8 +373,7 @@ Tensor & embedding_renorm_cuda_(Tensor & self, const Tensor & indices,
       unique_indices.data<int64_t>(),
       scalar_cast<accscalar_t>(max_norm),
       scalar_cast<accscalar_t>(norm_type),
-      dim);
-#endif
+      scalar_cast<int>(dim));
   });
   THCudaCheck(cudaGetLastError());
 
