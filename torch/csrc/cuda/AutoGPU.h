a29d2f39532ecaf9ff4f19659dcb68678afdcd9c
wsttiger <wsttiger@gmail.com>
2018-03-14 14:49:50 +0000

    added with WITH_ROCM preprocessor for constructor

--- torch/csrc/cuda/AutoGPU.h
+++ torch/csrc/cuda/AutoGPU.h
@@ -8,7 +8,7 @@
 class THP_CLASS THCPAutoGPU : public AutoGPU {
 public:
   explicit THCPAutoGPU(int device_id=-1) : AutoGPU(device_id) {}
-#ifdef WITH_CUDA
+#if defined(WITH_CUDA) || defined(WITH_ROCM)
   THCPAutoGPU(PyObject *args, PyObject *self=NULL);
   void setObjDevice(PyObject *obj);
 #endif
