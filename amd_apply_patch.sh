039293fec1df288e6fb433c5acfd716850ac43a1
Jorg Doku <jd728@cornell.edu>
2018-03-23 16:19:05 -0400

    Updated apply patch script

--- amd_apply_patch.sh
+++ amd_apply_patch.sh
@@ -2,6 +2,8 @@
 
 echo "Loading PyTorch AMD patch differentials."
 
+SETUP_PATCH_RESULT=$(git apply ../amd_patch_files/setup.patch 2>&1)
+
 cd torch
 TORCH_PATCH_RESULT=$(git apply ../amd_patch_files/torch.patch 2>&1)
 cd ..
@@ -9,6 +11,10 @@ cd ..
 cd aten
 ATEN_PATCH_RESULT=$((git apply ../amd_patch_files/aten.patch) 2>&1)
 cd ..
+
+if [ ! -z $SETUP_PATCH_RESULT]; then
+  echo "Failed to patch Setup:" $SETUP_PATCH_RESULT
+fi
 
 if [ ! -z $TORCH_PATCH_RESULT]; then
   echo "Failed to patch Torch:" $TORCH_PATCH_RESULT
@@ -18,12 +24,11 @@ if [ ! -z $ATEN_PATCH_RESULT]; then
   echo "Failed to patch ATen:" $ATEN_PATCH_RESULT
 fi
 
-if [[ $TORCH_PATCH_RESULT != ""  ||  $ATEN_PATCH_RESULT != "" ]]; then
+if [[ $TORCH_PATCH_RESULT != ""  ||  $ATEN_PATCH_RESULT != ""  || $SETUP_PATCH_RESULT != "" ]]; then
   echo "Failed to load AMD patch differentials."
 else
   echo "PyTorch AMD patch differentials successfully installed!"
 fi
-
 
 echo "Running the PyTorch hipify script."
 echo .
