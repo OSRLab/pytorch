79c5fbdd6e19f001eede48a30420091059cfa5ad
Jorg Doku <jd728@cornell.edu>
2018-04-04 13:37:57 -0700

    Removed inlining for constructor / destructor since HCC semi supports a call stack

--- aten/src/THC/THCDeviceUtils.cuh
+++ aten/src/THC/THCDeviceUtils.cuh
@@ -25,11 +25,7 @@ __host__ __device__ __forceinline__ T THCRoundUp(T a, T b) {
  * For CC 3.5+, perform a load using __ldg
  */
 template <typename T>
-#if defined(__HIP_PLATFORM_HCC__)
-__device__ __forceinline__ inline T doLdg(const T* p) {
-#else
 __device__ __forceinline__ T doLdg(const T* p) {
-#endif
 #if __CUDA_ARCH__ >= 350
   return __ldg(p);
 #else
@@ -37,11 +33,7 @@ __device__ __forceinline__ T doLdg(const T* p) {
 #endif
 }
 
-#if defined(__HIP_PLATFORM_HCC__)
-__device__ __forceinline__ inline unsigned int ACTIVE_MASK()
-#else
 __device__ __forceinline__ unsigned int ACTIVE_MASK()
-#endif
 {
 #if CUDA_VERSION >= 9000
     return __activemask();
@@ -51,11 +43,7 @@ __device__ __forceinline__ unsigned int ACTIVE_MASK()
 #endif
 }
 
-#if defined(__HIP_PLATFORM_HCC__)
-__device__ __forceinline__ inline int WARP_BALLOT(int predicate, unsigned int mask = 0xffffffff)
-#else
 __device__ __forceinline__ int WARP_BALLOT(int predicate, unsigned int mask = 0xffffffff)
-#endif
 {
 #if CUDA_VERSION >= 9000
     return __ballot_sync(mask, predicate);
@@ -84,11 +72,7 @@ __device__ __forceinline__ T WARP_SHFL_XOR(T value, int laneMask, int width = wa
 }
 
 template <typename T>
-#if defined(__HIP_PLATFORM_HCC__)
-__device__ __forceinline__ inline T WARP_SHFL(T value, int srcLane, int width = warpSize, unsigned int mask = 0xffffffff)
-#else
 __device__ __forceinline__ T WARP_SHFL(T value, int srcLane, int width = warpSize, unsigned int mask = 0xffffffff)
-#endif
 {
 #if CUDA_VERSION >= 9000
     return __shfl_sync(mask, value, srcLane, width);
@@ -98,11 +82,7 @@ __device__ __forceinline__ T WARP_SHFL(T value, int srcLane, int width = warpSiz
 }
 
 template <typename T>
-#if defined(__HIP_PLATFORM_HCC__)
-__device__ __forceinline__ inline T WARP_SHFL_UP(T value, unsigned int delta, int width = warpSize, unsigned int mask = 0xffffffff)
-#else
 __device__ __forceinline__ T WARP_SHFL_UP(T value, unsigned int delta, int width = warpSize, unsigned int mask = 0xffffffff)
-#endif
 {
 #if CUDA_VERSION >= 9000
     return __shfl_up_sync(mask, value, delta, width);
