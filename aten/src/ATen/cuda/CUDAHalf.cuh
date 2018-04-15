6147fea0215bb3a5af9a5eccc9acd448c15c4880
Jorg Doku <jd728@cornell.edu>
2018-04-15 16:56:12 -0700

    Removing hip proprocessors

--- aten/src/ATen/cuda/CUDAHalf.cuh
+++ aten/src/ATen/cuda/CUDAHalf.cuh
@@ -7,7 +7,6 @@
 #include <cuda_runtime.h>
 #include <cuda_fp16.h>
 
-#if !defined(__HIP_PLATFORM_HCC__)
 namespace at {
 template <> AT_API half convert(Half aten_half);
 template <> AT_API Half convert(half cuda_half);
@@ -17,4 +16,3 @@ template <> __half HalfFix(Half h);
 template <> Half HalfFix(__half h);
 #endif
 } // namespace at
-#endif
